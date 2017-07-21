45a68a045fe7f533d11676d56d32d48beacd7867
Charles <charliebill@btinternet.com>
2017-07-21 11:32:54 +0100

    Update ZHR stuff

--- lib/analysis/zhr/plotFinal.py
+++ lib/analysis/zhr/plotFinal.py
@@ -98,7 +98,7 @@ for shower in showers:
     """
 
     for year in okYears:
-        option = "all"
+        option = "peak"
         tuples.append((data[option][year][0],otherData[year][1]))
 
 # Peak (0.458444, 0.001750365)
@@ -108,15 +108,53 @@ for shower in showers:
 x = [float(i[0]) for i in tuples]
 y = [float(j[1]) for j in tuples]
 
-print(len(x))
-plt.scatter(x,y,color='b')
+mean = np.mean(x)
+std =np.std(x)
+
+xs = []
+ys = []
+
+for i in range(len(x)):
+    item = x[i]
+    if (item > mean+2*std) or (item < mean-2*std):
+        xs.append(item)
+
+for i in range(len(y)):
+    item = y[i]
+    if (item > mean+2*std) or (item < mean-2*std):
+        ys.append(item)
+
+x_new = []
+y_new = []
+
+x_outliers = []
+y_outliers = []
+
+for i,j in zip(x,y):
+    if (i in xs) or (j in ys):
+        x_outliers.append(i)
+        y_outliers.append(j)
+    else:
+        x_new.append(i)
+        y_new.append(j)
+
+print(x,y)
+print("==============")
+print(x_new,y_new)
+
+print(len(x_new))
+plt.scatter(x_new,y_new,color='b')
+plt.scatter(x_outliers, y_outliers, color="b",marker="+")
+
 plt.xlabel("Radio ZHR")
 plt.ylabel("Visual ZHR")
-plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), 'b:')
-#plt.scatter([np.mean(x)],[np.mean(y)], color='k')
-print(np.mean(x), np.mean(y))
 
-plt.savefig('/home/cwp/EMC/plots/zhr/figures/all_scatter.png',dpi=500)
+plt.plot(np.unique(x_new), np.poly1d(np.polyfit(x_new, y_new, 1))(np.unique(x_new)), 'b:')
+#plt.scatter([np.mean(x_new)],[np.mean(y_new)], color='k')
+print(np.mean(x_new), np.mean(y_new))
 
-print(np.corrcoef(x,y))
-print(stats.pearsonr(x,y))
+#plt.savefig('/home/cwp/EMC/plots/zhr/figures/all_scatter.png',dpi=500)
+plt.savefig('/home/cwp/ltx/papers/zhr/images/peak_scatter.png', dpi=500)
+
+print(np.corrcoef(x_new,y_new))
+print(stats.pearsonr(x_new,y_new))
